be2a3c701f46b25d18270eabab6e9631d6b1a1e3
Peter Wood <pwood@sankyinc.com>
2018-05-08 17:07:39 -0400

    Build basic API to handle player input, connections, and disconnects

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ var path = require('path');
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+var Game = require("./src/game.js").game;
+
 server.listen(PORT, () => {
   console.log('Server listening at port %d', PORT);
 });
@@ -37,7 +39,7 @@ function generatePlayerName() {
     "Diminuitive",
     "Harsh",
     "Majestic",
-    "Dull",
+    "Rampant",
     "Brilliant",
     "Distant",
     "Sword",
@@ -45,7 +47,7 @@ function generatePlayerName() {
     "Distraught",
     "Imaginary",
     "Alert",
-    "Cautious"
+    "Caution"
   ];
   const ANIMALS = [
     "Snake",
@@ -96,16 +98,86 @@ app.get("/", (request, response) => {
 /*
 Add player to existing game, or if no games, create a new game with the user as player 1
 */
+var roomNum = 0;
+var waitingGames = [];
 var games = [];
-var gamesCount = 0;
+
+// Run update loop for all games
+var intervalID = setInterval(() => {
+  for (var i = 0; i < games.length; i++) {
+    if (games[i].scene === "game") {
+      Game.update(games[i]);
+      io.to(games[i].room).emit("update", Game.getData(games[i]));
+    }
+  }
+}, Game.DELTA_TIME);
 
 io.on("connection", (socket) => {
-  console.log("User %s connected", socket.id);
   var name = generatePlayerName();
+  console.log("User %s connected: %s", socket.id, name);
+  var scene = "title"; // title, lobby, game
   var rerolls = REROLL_LIMIT;
-  socket.emit("name", name);
+  var game, gameIndex, playerNum, room;
 
-  socket.on("reroll name", (data) => {
+  socket.emit("welcome", { name: name, rerolls: rerolls, gamesAvailable: waitingGames.length > 0 });
+
+  function handleKeyDown(data) {
+    switch (scene) {
+      case "title":
+        scene = "lobby";
+        socket.emit("change scene", scene);
+        break;
+      case "lobby":
+      case "game":
+        if (game) {
+          switch (game.scene) {
+            case "game":
+              Game.handleKeyDown(game, playerNum, data.action);
+              break;
+          case "gameover":
+            // Rematch or return to lobby
+            switch (data.action) {
+              case "punch":
+                // Flag this player as wanting rematch, and only reset if both players are willing
+                Game.setWantsRematch(game, playerNum);
+                socket.broadcast.to(room).emit("wants rematch", playerNum);
+                if (game.wantsRematch[0] && game.wantsRematch[1]) {
+                  Game.reset(game);
+                  io.to(room).emit("rematch", Game.getData(game));
+                }
+                break;
+              case "reroll":
+                // Return player to lobby instead of rematching
+                io.to(room).emit("change scene", "lobby");
+                games.splice(gameIndex, 1);
+                console.log("Game count %d", games.length);
+                if (games.length === 0) {
+                  io.emit("no games available");
+                }
+                game = null;
+                break;
+            }
+            break;
+        }
+      }
+    }
+  }
+
+  function handleKeyUp(data) {
+    switch (scene) {
+      case "lobby":
+      case "game":
+        if (game) {
+          Game.handleKeyUp(game, playerNum, data.action);
+        }
+        break;
+    }
+  }
+
+  socket.on("keydown", handleKeyDown);
+  socket.on("keyup", handleKeyUp);
+
+  socket.on("reroll name", () => {
     if (rerolls > 0) {
       rerolls--;
       name = generatePlayerName();
@@ -116,14 +188,66 @@ io.on("connection", (socket) => {
     }
   });
 
-  socket.on("start multi", () => {
-    for (var i = 0; i < gamesCount; i++) {
-      // Look for a game awaiting a second player
+  socket.on("create game", () => {
+    console.log("create game");
+    // Don't allow the client to create a new game if there already is one
+    if (!game) {
+      // Create a new game
+      playerNum = 0;
+      room = (Date.now()).toString();
+      game = new Game(io, name, room);
+      console.log("Game room %d", game.room);
+      socket.join(room);
+      waitingGames.push(game);
+      gameIndex = waitingGames.length - 1;
+
+      socket.emit("game created", playerNum);
+      io.emit("games available");
     }
-    // Create a new game
+    else {
+      socket.emit("game exists", "Game already exists!");
+    }
+  });
+
+  socket.on("join game", () => {
+    // Look for a game awaiting a second player
+    for (var i = 0; i < waitingGames.length; i++) {
+      if (waitingGames[i]) {
+        playerNum = 1;
+        game = waitingGames.splice(i, 1)[0];
+        games.push(game);
+        gameIndex = i;
+        // Update all clients if this was the only waiting game
+        if (waitingGames.length === 0) {
+          io.emit("no games available");
+        }
+        room = game.room;
+        socket.join(room);
+        Game.addPlayer(game, name);
+        console.log("New game between %s and %s", game.names[0], game.names[1]);
+        // Send player number to client
+        socket.emit("player num", playerNum);
+        // Tell each client to start the game, and include the opponent's name
+        io.to(room).emit("start game", game.names);
+        break;
+      }
+    }
+  });
+
+  socket.on("opponent disconnected", () => {
+    // Client reports that opponent disconnected
+    // TODO: See whether we can communicate directly from the back-end socket to this one
+    //   so we don't have to rely on the client reporting the disconnect
+    game = null;
+    scene = "lobby";
   });
 
   socket.on("disconnect", () => {
     console.log("%s disconnected", name);
+    // Report disconnect to opponent
+    io.to(room).emit("opponent disconnected");
+    // Remove game from active games
+    games.splice(gameIndex, 1);
+    console.log("Game count %d", games.length);
   });
 });
